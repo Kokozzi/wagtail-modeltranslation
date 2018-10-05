50a2f1d99af106228d74c030a98b7d1bedfd51b6
fu2re <fu2re@yandex.ru>
2018-10-05 19:35:52 +0400

    translate caption

--- wagtail_modeltranslation/patch_wagtailadmin.py
+++ wagtail_modeltranslation/patch_wagtailadmin.py
@@ -140,8 +140,8 @@ class WagtailTranslator(object):
     def _patch_other_models(self, model):
         if hasattr(model, 'edit_handler'):
             edit_handler = model.edit_handler
-            for tab in edit_handler:
-                tab.children = self._patch_panels(tab.children)
+            # for tab in edit_handler:
+            #     tab.children = self._patch_panels(tab.children)
         elif hasattr(model, 'panels'):
             model.panels = self._patch_panels(model.panels)
         else:
